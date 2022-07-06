2a37bcc7e60dcc6fc32e3bda3fcb7aba445970c6
Gordon Ng <58711049+DrNogNog@users.noreply.github.com>
2022-07-06 14:56:51 -0400

    Update to code

--- Non-Decreasing.py
+++ Non-Decreasing.py
@@ -1,3 +1,15 @@
 #  4 3 3 4 
 # When decreasing numbers, always decrease it from the left to right, because
 # of the english words "non-decreasing"
+
+class Solution:
+    #check_Possibility is snake case
+    #this is camel case
+    def checkPossibility(self,nums: List[int]) -> bool:
+        #checks if the one number edit has been edited
+        changed = False
+        for i in range(len(nums) - 1):
+            if nums[i] <= nums[i+1]:
+                continue
+            if changed:
+                return False
