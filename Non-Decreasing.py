5f772f46cf1bfa2fe443fe5a4eed891e1745af34
Gordon Ng <58711049+DrNogNog@users.noreply.github.com>
2022-07-06 15:53:53 -0400

    Updated code

--- Non-Decreasing.py
+++ Non-Decreasing.py
@@ -9,7 +9,18 @@ class Solution:
         #checks if the one number edit has been edited
         changed = False
         for i in range(len(nums) - 1):
+            #end current iteration of loop and continues to next
             if nums[i] <= nums[i+1]:
                 continue
+            #checks if the one number edit works for the other numbers
             if changed:
                 return False
+            # [ 3 , 4 , 2 ]
+            # [i-1, i ,i+1]
+            # [ 3 , 3 , 2 , 4 ]
+            if i==0 or nums[i+1] >= nums[i-1]:
+                nums[i] = nums[i+1]
+            else:
+                nums[i+1] = nums[i]
+            changed = True
+        return True
