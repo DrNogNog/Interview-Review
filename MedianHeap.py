ea00492204dafa98b4c9400c54af7c24948b1bba
Gordon Ng <58711049+DrNogNog@users.noreply.github.com>
2022-07-07 15:20:49 -0400

    Median Heap Upload

--- MedianHeap.py
+++ MedianHeap.py
@@ -1,7 +1,7 @@
 # Max heap for smaller numbers
 # Min heap for bigger numbers 
 # Find Median
-
+import heapq, small, large
 class MedianFinder:
     def __init__(self):
         """
@@ -9,7 +9,21 @@ class MedianFinder:
         self.small, self.large = [], []
 
     def addNum(self, num:int) -> None:
-        heapq.heappush(self.small, -1 * num)
-        if (self.small and self.large and self.small[0])
+        heapq.heappush(small, -1 * num)
+        if (small and large and 
+            (-1 * self.small[0]) > self.large[0]):
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large,val)
+        if len(small) >len(large) + 1:
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large,val)
+        if len(large) > len(small) + 1:
+            val = heapq.heappop(self.large)
+            heapq.heappush(self.small, -1 * val)
 
     def findMedian(self) -> float:
+        if len(small) > len(large):
+            return self.small[0]
+        if len(large) > len(small):
+            return self.large[0]
+        return (self.small[0] + self.large[0]) / 2
