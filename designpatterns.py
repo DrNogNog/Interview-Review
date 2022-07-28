90f923404e32e2bbc0130f624d7bf838129ccfa3
Gordon Ng <58711049+DrNogNog@users.noreply.github.com>
2022-07-27 22:45:23 -0400

    named

---
+++ designpatterns.py
@@ -0,0 +1,31 @@
+# Notes on Design Patterns
+# Creational Patterns
+# Abstract Factory - Provide an interface for creating families of related or dependent objects without specifying their concrete classes.
+# Builder - Separate the construction of a complex object from its representation so that the
+# same construction process can create different representations
+# Factory Method - Define an interface for creating an object, but let subclasses decide which class to instantiate. Factory Method
+# lets a class defer instantiation to subclasses.
+# Prototype - Specify the kinds of objects to create using a prototypical instance, and
+# create new objects by copying this prototype.
+# Singleton - Ensure a class only has one instance, and provide a global point of access to it.
+
+# Structural Patterns
+# Adapter - Convert the interface of a class into another interface clients expect. Adapter 
+# lets classes work together that couldn't otherwise because of incompatible interfaces
+# Bridge decouple an abstraction from its implementation so that the two can vary independently
+# Composite - Compose objects into tree structures to represent part-whole hierarychies.
+# composite lets clients treat individuals objects and composition of objects uniformly
+# Decorator - Attach additional reposibilities to an object dynammically, decorators 
+# provide a flexible alternative to subclassing for extending functionality
+# Facade - Provide a unified interface to a set of interfaces in a subsystem. Facade defines a higher-level
+# interface making the subsystem easier to use.
+# Flyweight - Use sharing to support large numbers of fine-grained objects efficiently
+# Proxy - Provide a surrogate or placeholder for another object to control access to it.
+
+# Behavioral Patterns
+# Chain of Responsibility - Avoid coupling the sender of a request to its reciever by giving
+# more than one object a chance to handle the request. Chain the recieving objects
+# and pass the request along the chain until an object handles it.
+# Command - Encapsulate a request as an object, thereby letting you parameterize clients with different requests, queue
+# or log requests, and support undoable operations.
+# 
--- dijistras.py
+++ dijistras.py
@@ -1,9 +1,17 @@
-arr1 = [1,2,3,4,5]
-arr2 = map(lambda x: 2 * x, arr1)
-print(arr2)
+# import reduce
+# arr1 = [1,2,3,4,5]
+# arr2 = map(lambda x: 2 * x, arr1)
+# print(arr2)
 
-arr3 = reduce(lambda x,y: x+y, arr1)
+# arr3 = reduce(lambda x,y: x+y, arr1)
 
+#lambda is an anonymous function
 #bloomfilters - a guesstimate filter
 #hyperloglog- approximates the number of unique elements
 #Diffie-Hellman algorithm
+
+scifi_a = ["Isaac Asimov", "Ray Bradbury", "Robert Heinlein", "Arthur C. Clarke", "Douglas Adams"]
+scifi_a.sort(key=lambda x: x.split(" ")[0].lower())
+print(scifi_a)
+
+#dijistras finds the shortest path by means of weight on edges
